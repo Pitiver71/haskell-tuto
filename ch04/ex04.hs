b9025f4c13bb61507b357dcb83de9c2e748cbeb9
Jerome Fosse <jerome.fosse@ingenico.com>
2018-05-22 17:28:08 +0200

    beginning ch04

---
+++ ch04/ex04.hs
@@ -0,0 +1,17 @@
+import Data.List
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (x:xs) = Just xs
+
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast xs = Just $ last xs
+
+safeInit :: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit xs = Just $ init xs
