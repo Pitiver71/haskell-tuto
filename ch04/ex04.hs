7353443304a2212148cce24549ce54a98a253313
Jerome Fosse <jerome.fosse@ingenico.com>
2018-05-23 14:39:02 +0200

    new exercices in ch04

--- ch04/ex04.hs
+++ ch04/ex04.hs
@@ -15,3 +15,17 @@ safeLast xs = Just $ last xs
 safeInit :: [a] -> Maybe [a]
 safeInit [] = Nothing
 safeInit xs = Just $ init xs
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ [] = []
+splitWith p xs = loop [] [] xs
+    where loop curr acc [] = acc ++ [curr]
+          loop curr acc (x:xs) | not $ p x = loop (curr ++ [x]) acc xs
+                               | p x = loop [x] (newacc curr acc) xs
+                                    where newacc [] acc = acc
+                                          newacc curr acc = acc ++ [curr]
+
+firstWords :: String -> IO()
+firstWords str = printall $ map (\xs -> head xs) $ map words $ lines str
+                    where printall [] = return ()
+                          printall (x:xs) = putStrLn x >> printall xs
