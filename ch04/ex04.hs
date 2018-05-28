25b34f6f948e040b53d80ce45848a53da0d0bf9f
Jerome Fosse <jerome.fosse@ingenico.com>
2018-05-28 15:55:15 +0200

    still ch04

--- ch04/ex04.hs
+++ ch04/ex04.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char (digitToInt)
 
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
@@ -29,3 +30,49 @@ firstWords :: String -> IO()
 firstWords str = printall $ map (\xs -> head xs) $ map words $ lines str
                     where printall [] = return ()
                           printall (x:xs) = putStrLn x >> printall xs
+
+myMap :: ( a -> b) -> [a] -> [b]
+myMap _ [] = []
+myMap f xs = loop f xs []
+      where loop _ [] acc = acc
+            loop f (x:xs) acc = loop f xs $ acc ++ [(f x)]
+
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum xs = loop xs 0
+      where loop [] acc = acc
+            loop (x:xs) acc = loop xs $ acc + x
+
+
+asInt :: String -> Int
+asInt xs = loop 0 xs
+      where loop acc [] = acc
+            loop acc (x:xs) = let acc' = acc * 10 + digitToInt x
+                  in loop acc' xs            
+
+asInt2 :: String -> Int
+asInt2 [] = error "Empty String"
+asInt2 ['-'] = error "Not a number"
+asInt2 ('-':xs) = negate $ asInt2 xs
+asInt2 xs = foldl (\x y -> (x*10) + (digitToInt y)) 0 xs
+
+type ErrorMessage = String
+asIntEither :: String -> Either ErrorMessage Int
+asIntEither [] = Left "Empty String"
+asIntEither ['-'] = Left "Not a number"
+asIntEither ('-':xs) = Right $ negate $ asInt2 xs
+asIntEither xs = Right $ foldl (\x y -> (x*10) + (digitToInt y)) 0 xs
+
+concat2 :: [[a]] -> [a]
+concat2 xs = foldr (\x y -> x ++ y) [] xs
+
+takeWhile2 :: (a -> Bool) -> [a] -> [a]
+takeWhile2 _ [] = []
+takeWhile2 f xs = loop xs []
+      where loop [] acc = acc
+            loop (x:xs) acc | f x = loop xs (acc ++ [x])
+                            | not $ f x = acc
+
+takeWhile3 :: (a -> Bool) -> [a] -> [a]
+takeWhile3 f xs = foldr (\x acc -> if f x then x : acc else []) [] xs
+                            
